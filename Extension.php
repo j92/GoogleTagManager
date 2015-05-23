f6a4daef4e73eea68720bf20b62e5ea6a5ce0594
Tom Van Looy <tom@ctors.net>
2015-05-23 21:47:52 +0200

    Indent GTM code

--- Extension.php
+++ Extension.php
@@ -7,7 +7,6 @@ use Bolt\Extensions\Snippets\Location as SnippetLocation;
 
 class Extension extends \Bolt\BaseExtension
 {
-
     public function getName()
     {
         return "Google Tag Manager";
@@ -25,15 +24,16 @@ class Extension extends \Bolt\BaseExtension
         }
 
         $html = <<< EOM
-<!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=%gtm_container_id%"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','%gtm_container_id%');</script>
-<!-- End Google Tag Manager -->
+
+    <!-- Google Tag Manager -->
+    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=%gtm_container_id%"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','%gtm_container_id%');</script>
+    <!-- End Google Tag Manager -->
 EOM;
 
         $html = str_replace("%gtm_container_id%", $this->config['gtm_container_id'], $html);
