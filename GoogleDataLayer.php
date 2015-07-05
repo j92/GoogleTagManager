fd597e29333abd90f47b1327a3c5110f9d17e2cd
Joost van Driel <joostvdriel@gmail.com>
2015-07-05 13:46:49 +0000

    Class can now keep an array with data and convert it to script tag, which is needed for the Datalayer

--- GoogleDataLayer.php
+++ GoogleDataLayer.php
@@ -23,8 +23,18 @@ class GoogleDataLayer {
         $this->initialize();
     }
 
-    public function pushData(){
+    /**
+     * Push an array to the DataLayer
+     *
+     * @param array $data
+     */
+    public function pushData($data){
 
+        if( isset($data) ){
+            if(is_array($data) && !empty($data)){
+                array_push($this->data, $data);
+            }
+        }
     }
 
     public function removeData(){
@@ -35,7 +45,49 @@ class GoogleDataLayer {
 
     }
 
+    /**
+     * Convert data array to string
+     */
+    private function arrayToString($data){
+
+        if( empty($data) ){
+            return "";
+        }
+
+        // basis van datalayer
+        $data_layer = "";
+
+        // Loop door de data array en bouw dataLayer String
+        foreach( $data as $key => $value ){
+
+            $data_layer .= "'".$key."': ".$value.",\n";
+
+        }
+
+        // Remove last comma and /n
+        $data_layer = substr($data_layer, 0, -2);
+
+        return $data_layer;
+
+    }
+
+    /**
+     * Get the actual script tag with the current data in it
+     *
+     * @return string
+     */
     public function getDataLayerScript(){
-        
+
+        // basic of datalayer
+        $data_layer = "<script>dataLayer = [{ \n";
+
+        // Convert data to string
+        $data_layer .= $this->arrayToString($this->data);
+
+        // Finish datalayer
+        $data_layer .= "\n }]</script>";
+
+        return $data_layer;
+
     }
 }
